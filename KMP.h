1b7fb0a8acb3c3b23d0fd5caf4f9047cb051412a
Acciente717 <221thunder@gmail.com>
2017-01-21 20:50:42 +0800

    Update KMP.h

--- KMP.h
+++ KMP.h
@@ -25,7 +25,7 @@ void GenerateNextArray(char *str, int length, int* next)
 }
 
 // 返回pattern在target中的位置，不匹配则返回-1 
-int StringMatch(char *target, char *pattern)
+int strstr_KMP(char *target, char *pattern)
 {
 	int j = 0;
 	int patternLen = GetLength(pattern);
